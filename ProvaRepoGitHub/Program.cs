91e9593be2221f59a44b59f5d05a743ae4a6042f
arnau <arnau@DESKTOP-02LQMB1>
2024-01-16 19:36:51 +0100

    Added input MAIN/feature. Closes #1

--- ProvaRepoGitHub/Program.cs
+++ ProvaRepoGitHub/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+namespace provaRepoGitHub
+{
+    public class provaRepoGitHubProj
+    {
+        public static void Main()
+        {
+            int num;
+            const string MsgInput = "Introdueix un numero per comprovar si es parell o no:";
+            Console.WriteLine(MsgInput);
+            num=Convert.ToInt32(Console.ReadLine());
+
+
+        }
+
+    }
+}
